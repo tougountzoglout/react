bcb31fbed7454dc215f3921993cbad9db0103ac9
theofilos <teo414@hotmail.com>
2020-03-12 14:36:26 +0200

    big  2

--- src/main/java/teo/spring/react/ReactApplication.java
+++ src/main/java/teo/spring/react/ReactApplication.java
@@ -2,6 +2,11 @@ package teo.spring.react;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
 public class ReactApplication {
@@ -9,5 +14,19 @@ public class ReactApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReactApplication.class, args);
     }
+
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+
+                registry.addMapping("/**").allowedOrigins("*"); // for /** means all mapping URL, and * for all domain
+
+            }
+        };
+    }
+
 
 }
